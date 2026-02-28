b0572fc19ecf1cfe7f85dd1518eeeeae8095647b
bigman1313000-star <bigman1313000@gmail.com>
2026-02-27 23:19:14 -0600

    Add files via upload

---
+++ buttonclicked.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("gohome");
+
+button.addEventListener("click", function() {
+  window.location.href = "index.html";
+})
---
+++ index.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("link");
+
+button.addEventListener("click", function() {
+  window.location.href = "buttonclicked.html";
+});
